8b4782f279a5fbdf9e182aab70a17b1e73384c97
Mae Beale <maebeale@gmail.com>
2020-04-01 09:21:36 -0400

    Committing the merged/latest/greatest schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_105929) do
+ActiveRecord::Schema.define(version: 2020_04_01_123807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

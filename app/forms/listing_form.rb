b49bf35905a8741e4a834e8e5acf026eefcea989
exbinary <exbinary@gmail.com>
2020-03-31 23:40:01 -0400

    Add defaul tags to ListingForm

--- app/forms/listing_form.rb
+++ app/forms/listing_form.rb
@@ -1,5 +1,30 @@
 class ListingForm < Reform::Form
+  DEFAULT_TAGS = [
+    'prepared meals',
+    'groceries',
+    'household supplies',
+    'cleaning',
+    'laundry',
+    'home repairs',
+    'yard work',
+    'childcare',
+    'dog walking',
+    'pet sitting',
+    'emotional support',
+    'religious support',
+    'errands',
+    'transportation',
+    'making calls',
+    'tech support',
+    'translation',
+    'cash',
+  ]
+
   collection :tags
 
   property :location, form: LocationForm
+
+  def self.all_tags
+    DEFAULT_TAGS | Listing.all_tags
+  end
 end

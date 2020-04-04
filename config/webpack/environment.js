6377d68915a10363edbba760b5d75462467374f6
exbinary <exbinary@gmail.com>
2020-04-04 11:22:22 -0400

    webpacker:install:vue

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,6 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 const webpack = require('webpack')
 
 environment.plugins.prepend('Provide',
@@ -9,4 +11,6 @@ environment.plugins.prepend('Provide',
   })
 )
 
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment

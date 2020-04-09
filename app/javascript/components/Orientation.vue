9d750f168ff7cd90b4a71c41cc42addec3d74bb9
Ryan <rydowd@gmail.com>
2020-04-09 00:13:33 -0400

    added links to intake and external resource forms

--- app/javascript/components/Orientation.vue
+++ app/javascript/components/Orientation.vue
@@ -5,13 +5,25 @@
     <AboutSection title="Where do you fit in?" description="If there are ways that you'd like to contribute to bettering the lives of the community around you. If there are needs of yours that your community could help you meet. Whatever your individual case may be, we're here to help you make that connection." />
     <div class='columns'>
       <div class='column'>
-        
+        <div>
+          <h2 class='subtitle is-4'>Want to help?</h2>
+        </div>
+        <div>
+          <p>Let us know how you'd like to <a href='/listings'>contribute</a>.</p>
+        </div>
       </div>
       <div class='column'>
-        
+        <div>
+          <h2 class='subtitle is-4'>Need some help?</h2>
+        </div>
+        <div>
+          <p>How can our network <a href='/listings'>help you</a>?</p>
+        </div>
       </div>
       <div class='column'>
-        
+        <div>
+          <h2 class='subtitle is-4'><a href='/external_resources'>Is there an outside resource we should know about?</a></h2>
+        </div>
       </div>
     </div>
   </div>
@@ -29,3 +41,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .column div{
+    text-align:center;
+  }
+</style>

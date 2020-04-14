1b5982d257aec717dbbd340a5aae9e7f6784cc3b
Howard M. Miller <hmiller@customink.com>
2020-04-14 11:39:19 -0400

    move listings page into vue

--- app/javascript/components/index.js
+++ app/javascript/components/index.js
@@ -1,9 +1,11 @@
 import HelloWorld from './HelloWorld'
 import Orientation from './Orientation'
 import AboutSection from './AboutSection'
+import PublicListings from './PublicListings'
 
 export {
   HelloWorld,
   Orientation,
   AboutSection,
+  PublicListings
 }

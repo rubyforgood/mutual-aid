ecf9acb54366a4141738882bd70292d7814d005a
Mae Beale <maebeale@gmail.com>
2020-04-18 19:26:27 -0400

    Remove comments

--- db/migrate/20200412132525_change_website_display_fields.rb
+++ db/migrate/20200412132525_change_website_display_fields.rb
@@ -5,18 +5,3 @@ class ChangeWebsiteDisplayFields < ActiveRecord::Migration[6.0]
     add_column :external_resources, :display_on_website_end, :date
   end
 end
-
-# create_table :system_tags do |t|
-#   t.references :organization, null: true, index: true, foreign_key: true
-#   t.references :parent, null: true, index: true, foreign_table_name: :system_tags
-#   t.string :parent_type, null: true, index: true
-#
-#   t.string :name, null: false, index: true
-#   t.string :description, index: true
-#   t.boolean :display_to_public, null: false, default: true, index: true
-#   t.integer :display_order, null: false, default: 10, index: true
-#
-#   t.string :created_by
-#
-#   t.timestamps
-# end

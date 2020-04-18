2197ea418359ced585d5e3b41c7897edc1336360
Mae Beale <maebeale@gmail.com>
2020-04-18 19:01:41 -0400

    Add devise helpers for admin vs user vs unauthenticated

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -49,7 +49,11 @@ RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Warden::Test::Helpers
 
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
@@ -69,4 +73,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  #
 end

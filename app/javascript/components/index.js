d5492867bf639ccb7ceb8ecb29fc0214a7df9e3c
exbinary <exbinary@gmail.com>
2020-04-21 01:35:41 -0400

    Remove HelloWorld.vue example now that we have others

--- app/javascript/components/index.js
+++ app/javascript/components/index.js
@@ -1,10 +1,8 @@
-import HelloWorld from './HelloWorld'
 import Orientation from './Orientation'
 import AboutSection from './AboutSection'
 import PublicListings from './PublicListings'
 
 export {
-  HelloWorld,
   Orientation,
   AboutSection,
   PublicListings
--- app/javascript/entry_points/index.js
+++ app/javascript/entry_points/index.js
@@ -1,11 +1,9 @@
-import hello from './hello'
 import navBar from './navBar'
 import notice from './notice'
 import orientation from './orientation'
 import publicListings from './publicListings'
 
 export {
-  hello,
   navBar,
   notice,
   orientation,

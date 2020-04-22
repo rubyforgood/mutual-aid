77860b51ad2d161d031c5704575359a6a850c373
Howard M. Miller <hmiller@customink.com>
2020-04-22 14:15:05 -0400

    add basic JS for the grid and list browsers
    also move the JS test directory from `test/` to `spec/javascript`

---
+++ app/javascript/components/ListBrowser.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="listBrowser">
+    <table class='table table-hover table-curved table-condensed is-hoverable'>
+      <tr>
+        <th>Categories</th>
+        <th>Timing</th>
+        <th>Availability</th>
+        <th>Location</th>
+        <th>Details</th>
+      </tr>
+      <tr v-for="listing in listings" :key="listing.id">
+        <td>{{ listing.tegories }}</td>
+        <td>{{ listing.timings}}</td>
+        <td>{{ listing.availability}}</td>
+        <td>{{ listing.location}}</td>
+        <td><!-- details component goes here --></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    listings: {type: Array},
+    filters: {type: Object},
+  }
+}
+</script>
--- spec/javascript/setup.js
+++ spec/javascript/setup.js
@@ -1,1 +1,2 @@
 require('jsdom-global')()
+require('chai/register-assert')

2984639ac7b9085f4b73236627a6ac7681f86eef
exbinary <exbinary@gmail.com>
2020-04-23 15:13:31 -0400

    Spec BrowserSelector button de|activation

--- spec/javascript/pages/Browse.spec.js
+++ spec/javascript/pages/Browse.spec.js
@@ -10,16 +10,33 @@ describe('Browse', function() {
   })
 
   describe('browser view', function() {
-    it('renders the Tile view by default', function() {
-      assert.isTrue(this.wrapper.contains(TileBrowser))
+    describe('starts in Tile view', function() {
+      it('renders the TileBrowser', function() {
+        assert.isTrue(this.wrapper.contains(TileBrowser))
+        assert.isFalse(this.wrapper.contains(ListBrowser))
+      })
+
+      it('disables the Tile view button', function() {
+        assert.equal(this.wrapper.find('button#show-tiles').attributes('disabled'), 'disabled')
+        assert.notExists(this.wrapper.find('button#show-list').attributes('disabled'))
+      })
     })
 
-    it('can be switched to the List view', async function() {
-      this.wrapper.find('button#show-list').trigger('click')
-      await this.wrapper.vm.$nextTick()
+    describe('when switched to the List view', function() {
+      beforeEach(async function() {
+        this.wrapper.find('button#show-list').trigger('click')
+        await this.wrapper.vm.$nextTick()
+      })
 
-      assert.isTrue(this.wrapper.contains(ListBrowser))
-      assert.isFalse(this.wrapper.contains(TileBrowser))
+      it('renders the ListBrowser', function() {
+        assert.isTrue(this.wrapper.contains(ListBrowser))
+        assert.isFalse(this.wrapper.contains(TileBrowser))
+      })
+
+      it('disables the List view button', function() {
+        assert.equal(this.wrapper.find('button#show-list').attributes('disabled'), 'disabled')
+        assert.notExists(this.wrapper.find('button#show-tiles').attributes('disabled'))
+      })
     })
   })
 })

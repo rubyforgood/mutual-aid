98c9d223ae06b5eca45a181a640cca10a4705221
exbinary <exbinary@gmail.com>
2020-04-23 00:45:01 -0400

    Allow `yarn test` to run alongside `bin/webpack-dev-server`
    Currently, running tests will break the dev-server because the testing
    build chain writes to the same `manifest.json` file used by the
    dev-server, but with incompatible pack paths.
    
    This configures the test env to not write the manifest to disk.
    
    I'm not sure if there are other unintended side-effects to this change.
    An alternative if this doesn't pan out is to instead change the
    `publicPath` the manifest is written out to.

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,6 +1,15 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'test'
 
 const environment = require('./environment')
-var babelLoader = environment.loaders.get('babel')
+
+const babelLoader = environment.loaders.get('babel')
 babelLoader.include.push(babelLoader.include[0].concat('../../test'))
+
+// Don't write manifest to disk in test env; it would write out pack paths that
+// webpack-dev-server doesn't like.
+// Alternatively, we could change the `publicPath` property so it writes somewhere else?
+// Somewhat related https://github.com/rails/webpacker/issues/435
+const manifestPlugin = environment.plugins.get('Manifest');
+manifestPlugin.options.writeToDisk = false
+
 module.exports = environment.toWebpackConfig()

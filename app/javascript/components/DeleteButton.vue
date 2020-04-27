f651398ada0ca76af2583064aaa646b4dd1da8cf
exbinary <exbinary@gmail.com>
2020-04-27 19:58:30 -0400

    Extract reusable DeleteButton component
    Generate the same markup as rails `button_to ..., method: :delete`.

---
+++ app/javascript/components/DeleteButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <form :action="action" method="post">
+    <input type="hidden" name="_method" value="delete" />
+    <input type="hidden" name="authenticity_token" :value="authenticityToken" />
+    <button type="submit" class="button is-outlined">
+      <slot />
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    action: String,
+  },
+  computed: {
+    authenticityToken() {
+      return document.querySelector('meta[name="csrf-token"]').content
+    },
+  },
+}
+</script>
--- app/javascript/components/NavBar.vue
+++ app/javascript/components/NavBar.vue
@@ -5,21 +5,19 @@
         <img :src="$options.logo" alt="festi logo" height="300px">
       </b-navbar-item>
     </template>
+
     <template slot="start">
       <b-navbar-item href="/about">About</b-navbar-item>
       <b-navbar-item href="/share">Share</b-navbar-item>
       <b-navbar-item href="/community_resources_list">Community Resources</b-navbar-item>
       <b-navbar-item href="/news">Announcements</b-navbar-item>
     </template>
+
     <template slot="end" v-if="loggedIn">
       <b-navbar-item href="/users/edit">Account</b-navbar-item>
       <b-navbar-item href="/admin">Admin</b-navbar-item>
       <b-navbar-item tag="div">
-        <form action="/users/sign_out" method="post">
-          <input type="hidden" name="_method" value="delete" />
-          <input type="hidden" name="authenticity_token" :value="authenticityToken" />
-          <button type="submit" class="button is-outlined">Logout</button>
-        </form>
+        <DeleteButton action="users/sign_out">Logout</DeleteButton>
       </b-navbar-item>
     </template>
     <template slot="end" v-else>
@@ -32,17 +30,16 @@
 
 <script>
 import logo from 'images/logo.png'
+import DeleteButton from 'components/DeleteButton'
 
 export default {
-  logo: logo,
   props: {
     loggedIn: Boolean,
   },
-  computed: {
-    authenticityToken() {
-      return document.querySelector('meta[name="csrf-token"]').content
-    },
+  components: {
+    DeleteButton,
   },
+  logo: logo,
 }
 </script>
 

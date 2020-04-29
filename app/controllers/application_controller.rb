9cc44461f1d7a6fe28a88c7c2e056562f8fe42c6
Mae Beale <maebeale@gmail.com>
2020-04-28 21:18:41 -0400

    Allow url params to toggle locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :admin_status
+  around_action :switch_locale
 
   def admin_status
     @admin_status = params[:admin] ? YAML.load(params[:admin]) : current_user&.admin? # allows admin user to simulate with param=false
+  end
+
+  def switch_locale(&action)
+    locale = params[:locale] || current_user.try(:locale) || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 
 end

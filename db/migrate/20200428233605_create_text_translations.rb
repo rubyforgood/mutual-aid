f85e392fdfc558255e75ec9801c837e49ab7080d
Mae Beale <maebeale@gmail.com>
2020-04-28 21:18:18 -0400

    Run Mobility install generator

---
+++ db/migrate/20200428233605_create_text_translations.rb
@@ -0,0 +1,14 @@
+class CreateTextTranslations < ActiveRecord::Migration[6.0]
+
+  def change
+    create_table :mobility_text_translations do |t|
+      t.string :locale, null: false
+      t.string :key,    null: false
+      t.text :value
+      t.references :translatable, polymorphic: true, index: false
+      t.timestamps null: false
+    end
+    add_index :mobility_text_translations, [:translatable_id, :translatable_type, :locale, :key], unique: true, name: :index_mobility_text_translations_on_keys
+    add_index :mobility_text_translations, [:translatable_id, :translatable_type, :key], name: :index_mobility_text_translations_on_translatable_attribute
+  end
+end
---
+++ db/migrate/20200428233606_create_string_translations.rb
@@ -0,0 +1,15 @@
+class CreateStringTranslations < ActiveRecord::Migration[6.0]
+
+  def change
+    create_table :mobility_string_translations do |t|
+      t.string :locale, null: false
+      t.string :key, null: false
+      t.string :value
+      t.references :translatable, polymorphic: true, index: false
+      t.timestamps null: false
+    end
+    add_index :mobility_string_translations, [:translatable_id, :translatable_type, :locale, :key], unique: true, name: :index_mobility_string_translations_on_keys
+    add_index :mobility_string_translations, [:translatable_id, :translatable_type, :key], name: :index_mobility_string_translations_on_translatable_attribute
+    add_index :mobility_string_translations, [:translatable_type, :key, :value, :locale], name: :index_mobility_string_translations_on_query_keys
+  end
+end

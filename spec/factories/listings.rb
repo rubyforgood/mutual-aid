199710cebe492e10a391da38a59cd29e74abf815
Mae Beale <maebeale@gmail.com>
2020-05-01 19:25:08 -0400

    Update specs to reflect model changes

--- spec/factories/listings.rb
+++ spec/factories/listings.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :listing do
     type { %w(Offer Ask).sample }
+    association :person
+    association :service_area
+
     trait :with_location do
       location
     end
--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -1,7 +1,32 @@
 FactoryBot.define do
   factory :person do
-    user
     first_name { Faker::Name.name }
     last_name { Faker::Name.name }
+    email { Faker::Internet.email }
+
+    trait :with_email do
+      email { Faker::Internet.email }
+    end
+
+    trait :with_phone do
+      phone { Faker::PhoneNumber.phone_number }
+    end
+
+    trait :with_email_and_phone do
+      email { Faker::Internet.email }
+      phone { Faker::PhoneNumber.phone_number }
+    end
+
+    trait :with_location do
+      association :location
+    end
+
+    trait :with_location do
+      association :location
+    end
+
+    trait :with_user do
+      association :user
+    end
   end
 end

18f565cf915a66e477d0539567fb975ba7772de9
Mae Beale <maebeale@gmail.com>
2020-05-02 21:41:28 -0400

    Update person factory to include preferred_contact_method info

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -2,17 +2,21 @@ FactoryBot.define do
   factory :person do
     first_name { Faker::Name.name }
     last_name { Faker::Name.name }
+    preferred_contact_method { "email" }
     email { Faker::Internet.email }
 
     trait :with_email do
+      preferred_contact_method { "email" }
       email { Faker::Internet.email }
     end
 
     trait :with_phone do
+      preferred_contact_method { "phone" }
       phone { Faker::PhoneNumber.phone_number }
     end
 
     trait :with_email_and_phone do
+      preferred_contact_method { "email" }
       email { Faker::Internet.email }
       phone { Faker::PhoneNumber.phone_number }
     end

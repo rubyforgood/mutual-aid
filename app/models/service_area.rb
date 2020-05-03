b2a9f4da638920f1abe0c062a7553d476c54e971
Mae Beale <maebeale@gmail.com>
2020-05-02 21:20:48 -0400

    Correct model name for service_area assns

--- app/models/service_area.rb
+++ app/models/service_area.rb
@@ -6,8 +6,8 @@ class ServiceArea < ApplicationRecord
   belongs_to :parent, class_name: "ServiceArea", inverse_of: :service_areas, optional: true
   belongs_to :organization, optional: true
 
-  has_many :asks, class_name: "Listing::Ask", foreign_key: "service_area_id", inverse_of: :service_area
-  has_many :offers, class_name: "Listing::Offer", foreign_key: "service_area_id", inverse_of: :service_area
+  has_many :asks, class_name: "Ask", foreign_key: "service_area_id", inverse_of: :service_area
+  has_many :offers, class_name: "Offer", foreign_key: "service_area_id", inverse_of: :service_area
   has_many :listings
   has_many :people
   has_many :service_areas, inverse_of: :parent

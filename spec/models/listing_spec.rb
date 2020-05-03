b75346e38d7c0b9aac776eb5378ca69b42f88134
Mae Beale <maebeale@gmail.com>
2020-05-02 21:42:11 -0400

    Remove one extra create from listing_spec -- not sure why person can't be removed.

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Listing, type: :model do
-  let(:listing) { build(:offer, person: create(:person), service_area: create(:service_area)) }
+  let(:listing) { build(:offer, person: create(:person)) }
 
   describe 'tagging' do
     example 'smoke test' do

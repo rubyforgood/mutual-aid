99257c73fa39a1ef329274da8eb4db9af7353ddf
Mae Beale <maebeale@gmail.com>
2020-05-04 09:03:38 -0400

    Add constants to communication_log.rb to support form (and later reference from elsewhere in the system)

--- app/models/communication_log.rb
+++ app/models/communication_log.rb
@@ -3,4 +3,7 @@ class CommunicationLog < ApplicationRecord
   belongs_to :match, optional: true
   belongs_to :created_by, optional: true, class_name: "User", foreign_key: "created_by_id"
 
+  DEFAULT_DELIVERY_STATUS = "completed"
+  AUTO_DELIVERY_CHANNEL = "autoemail"
+  DELIVERY_CHANNELS = [AUTO_DELIVERY_CHANNEL, "email", "text", "call", "snailmail"]
 end

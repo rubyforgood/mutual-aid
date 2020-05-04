40bf4ca6050f85ac7ecdf7f7ec61141e072f6bee
Mae Beale <maebeale@gmail.com>
2020-05-04 09:27:29 -0400

    Add defaults/constants/seeds for form-related contacts (referenced in autoemails)

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,5 +1,14 @@
 class Position < ApplicationRecord
   belongs_to :organization
-  belongs_to :person
+  belongs_to :person, optional: true
 
+  ASK_FORM_CONTACT_TITLE = "ASK_FORM_CONTACT"
+  OFFER_FORM_CONTACT_TITLE = "OFFER_FORM_CONTACT"
+  COMMUNITY_RESOURCES_CONTACT_TITLE = "COMMUNITY_RESOURCES_CONTACT"
+  FORM_CONTACT_TITLES = [ASK_FORM_CONTACT_TITLE, OFFER_FORM_CONTACT_TITLE,
+                         COMMUNITY_RESOURCES_CONTACT_TITLE, "Point of contact", "Member"]
+
+  def name
+    "#{person.name + ", " if person.present?}#{position_type} "
+  end
 end

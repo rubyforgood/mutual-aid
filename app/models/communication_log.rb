1a7d3e171146832feb15e18aa5b3745b2bc52b88
Mae Beale <maebeale@gmail.com>
2020-05-04 08:51:17 -0400

    Add fields to CommunicationLog in preparation of creating Submission-related email logs

--- app/models/communication_log.rb
+++ app/models/communication_log.rb
@@ -1,5 +1,6 @@
 class CommunicationLog < ApplicationRecord
   belongs_to :person, optional: true
   belongs_to :match, optional: true
+  belongs_to :created_by, optional: true, class_name: "User", foreign_key: "created_by_id"
 
 end

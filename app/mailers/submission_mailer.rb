87e040cfdcc796ca833e6c7d3d7b88c6ae6538dc
Mae Beale <maebeale@gmail.com>
2020-05-04 17:12:42 -0400

    Refactor mailer

--- app/mailers/submission_mailer.rb
+++ app/mailers/submission_mailer.rb
@@ -6,13 +6,16 @@ class SubmissionMailer < ApplicationMailer
   def new_submission_confirmation_email(submission)
     @submission = submission
 
+    current_organization = Organization.current_organization
+    @organization_name = current_organization.name
+
     @form_name = @submission.form_name
     if @form_name.downcase.include?("ask")
-      @form_contact = Organization.current_organization.ask_form_contact
+      @form_contact = current_organization.ask_form_contact
     elsif @form_name.downcase.include?("offer")
-      @form_contact = Organization.current_organization.offer_form_contact
+      @form_contact = current_organization.offer_form_contact
     elsif @form_name.downcase.include?("community_resource")
-      @form_contact = Organization.current_organization.community_resources_contact
+      @form_contact = current_organization.community_resources_contact
     end
 
     @person = @submission.person
@@ -20,15 +23,19 @@ class SubmissionMailer < ApplicationMailer
 
     @system_settings = SystemSetting.current_settings
 
-    system_email = "#{ENV["SYSTEM_EMAIL"]}"
-    email_with_name = %("#{system_email}" <#{system_email}>)
+    system_email = ENV["SYSTEM_EMAIL"]
+    sendgrid_from_email = ENV["SENDGRID_FROM_EMAIL"]
+    contact_email = @form_contact&.person&.email || "#{sendgrid_from_email}"
+    contact_name =  @form_contact&.person&.name || @form_contact&.name || contact_email
+    contact_email_with_name = %("#{contact_name} (#{Organization.current_organization.name})" <#{contact_email}>)
+    bcc_emails = [contact_email, sendgrid_from_email, system_email].uniq.join("; ")
 
     @subject =  "#{ENV["SYSTEM_APP_NAME"]} confirmation (" + @person.updated_at.to_date.to_s + ")"
 
     mail(to: @person.email,
-         from: email_with_name,
-         bcc: system_email,
-         reply_to: email_with_name.to_s + ", " + system_email.to_s,
+         from: contact_email_with_name,
+         bcc: bcc_emails,
+         reply_to: contact_email_with_name.to_s + ", " + system_email.to_s,
          subject: @subject) do |format|
       format.html { render template_path: 'submission_mailer',
                            template_name: 'new_submission_confirmation_email' }

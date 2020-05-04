f9bd92a93f2bea0f161824928e45b8a3dc272840
Mae Beale <maebeale@gmail.com>
2020-05-04 09:28:26 -0400

    WIP: add helpers for system_settings -- this needs review

--- app/models/system_setting.rb
+++ app/models/system_setting.rb
@@ -1,2 +1,56 @@
 class SystemSetting < ApplicationRecord
+
+  EXCHANGE_TYPES = [
+      "fully_moderated", # no public access
+      "dispatch_moderated",  # peer-to-peer, but
+      "moderation_on_request", # option on Forms to request hidden from public and/or messaging moderation
+      "dispatch_assisted",  # peer-to-peer, but admins (dispatch/connectors/coordinators) can also complete matches
+      "peer_to_peer" # everything is public
+  ]
+
+  # everything is public, including contact info
+  # everything is public, but messaging is deidentified
+  # things are optionally public, but request to connect is moderated by dispatch
+  # things are optionally public, and deidentified messaging is an option
+  #
+  ### contribution display settings
+  # all contributions are public
+  # contributions are optionally public
+  # no contributions are public
+  # all contributions are visible to people with accounts
+  # some contributions are visible to people with accounts
+  #
+  ### messaging settings
+  # people are able to find each other's contact info
+  # people are able to message each other (deidentified, a la craigslist)
+  # people are able to message each other, but can request messages be tracked/moderated
+  # people are not able to contact each other, but can request to be connected with specific contributions
+  # people only contact dispatch
+  #
+
+  def self.current_settings
+    self.last
+  end
+
+
+  def dispatch_assisted?
+    exchange_type == "dispatch_assisted"
+  end
+
+  def dispatch_moderated?
+    exchange_type == "dispatch_moderated"
+  end
+
+  def fully_moderated?
+    exchange_type == "fully_moderated"
+  end
+
+  def moderation_on_request?
+    exchange_type == "moderation_on_request"
+  end
+
+  def peer_to_peer?
+    exchange_type == "peer_to_peer"
+  end
+
 end

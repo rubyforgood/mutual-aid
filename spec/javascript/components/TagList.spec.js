379ca3ad0fa79d9c658b07714502004aeb8012d7
exbinary <exbinary@gmail.com>
2020-05-05 20:59:18 -0400

    Ensure all specs have a top-level describe block
    Helps group spec output so its easy to tell which spec file an example
    belongs to.
    
    Note: the diffs here are essentially whitespace-only, except for said
    describe blocks.

--- spec/javascript/components/TagList.spec.js
+++ spec/javascript/components/TagList.spec.js
@@ -2,34 +2,36 @@ import {assert} from 'chai'
 import {shallowMount} from '@vue/test-utils'
 import TagList from 'components/TagList'
 
-it('has default css classes', function () {
-  const wrapper = shallowMount(TagList, {
-    propsData: {
-      tags: [{id: 1, name: 'a'}],
-    },
+describe('TagList', () => {
+  it('has default css classes', function () {
+    const wrapper = shallowMount(TagList, {
+      propsData: {
+        tags: [{id: 1, name: 'a'}],
+      },
+    })
+    assert.deepEqual(wrapper.classes(), ['tagList', 'tags'])
+    assert.deepEqual(wrapper.find('li').classes(), ['tagList-tag', 'tag', 'is-info', 'is-light'])
   })
-  assert.deepEqual(wrapper.classes(), ['tagList', 'tags'])
-  assert.deepEqual(wrapper.find('li').classes(), ['tagList-tag', 'tag', 'is-info', 'is-light'])
-})
 
-it('can override some of the default classes', function () {
-  const wrapper = shallowMount(TagList, {
-    propsData: {
-      tags: [{id: 2, name: 'b'}, [{id: 3, name: 'c'}]],
-      tagClasses: 'childClass',
-    },
+  it('can override some of the default classes', function () {
+    const wrapper = shallowMount(TagList, {
+      propsData: {
+        tags: [{id: 2, name: 'b'}, [{id: 3, name: 'c'}]],
+        tagClasses: 'childClass',
+      },
+    })
+    assert.deepEqual(wrapper.findAll('li').at(0).classes(), ['tagList-tag', 'tag', 'childClass'])
+    assert.deepEqual(wrapper.findAll('li').at(1).classes(), ['tagList-tag', 'tag', 'childClass'])
   })
-  assert.deepEqual(wrapper.findAll('li').at(0).classes(), ['tagList-tag', 'tag', 'childClass'])
-  assert.deepEqual(wrapper.findAll('li').at(1).classes(), ['tagList-tag', 'tag', 'childClass'])
-})
 
-it('renders empty if the tags are invalid', function () {
-  const nullWrapper = shallowMount(TagList, {
-    propsData: {tags: null},
+  it('renders empty if the tags are invalid', function () {
+    const nullWrapper = shallowMount(TagList, {
+      propsData: {tags: null},
+    })
+    const emptyWrapper = shallowMount(TagList, {propsData: {tags: []}})
+    const nullEmptyWrapper = shallowMount(TagList, {propsData: {tags: [null]}})
+    assert.isTrue(nullWrapper.isEmpty())
+    assert.isTrue(emptyWrapper.isEmpty())
+    assert.isTrue(nullEmptyWrapper.isEmpty())
   })
-  const emptyWrapper = shallowMount(TagList, {propsData: {tags: []}})
-  const nullEmptyWrapper = shallowMount(TagList, {propsData: {tags: [null]}})
-  assert.isTrue(nullWrapper.isEmpty())
-  assert.isTrue(emptyWrapper.isEmpty())
-  assert.isTrue(nullEmptyWrapper.isEmpty())
 })

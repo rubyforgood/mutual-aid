bd0b175ccbb37a6d8cf794b1e8e56a709bbc91e4
exbinary <exbinary@gmail.com>
2020-05-05 21:22:46 -0400

    Add composition api

--- app/javascript/vue_config.js
+++ app/javascript/vue_config.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Buefy from 'buefy'
+import CompositionApi from '@vue/composition-api'
 
 export function configure(vue) {
+  Vue.use(CompositionApi)
   vue.use(Buefy, {
     defaultIconPack: 'fas',
   })
-
   return vue
 }
 

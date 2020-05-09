5c0ba3724056d0739665575d83cb5db85e00726a
exbinary <exbinary@gmail.com>
2020-05-09 10:54:23 -0400

    Add minimal location fields to Offer.vue

---
+++ app/javascript/pages/offer/LocationFields.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-field :label-for="withPrefix('street_address')" label="Street address">
+      <b-input :name="withPrefix('street_address')" :value="street_address" />
+    </b-field>
+
+    <!-- TODO: remove this with the DB constraint on state and replace with `type` (maybe in the controller?) -->
+    <b-input :name="withPrefix('state')" type="hidden" value="MI" />
+  </div>
+</template>
+
+<script>
+import {partial} from 'utils/function'
+import {fieldNameWithPrefix} from 'utils/form'
+
+export default {
+  props: {
+    fieldNamePrefix: String,
+    street_address: String,
+  },
+  created: function() {
+    // TODO: tiny bit of duplication with partial+fieldNameWithPrefix
+    this.withPrefix = partial(fieldNameWithPrefix, this.fieldNamePrefix)
+  },
+}
+</script>

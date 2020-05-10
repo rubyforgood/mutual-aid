9c10a75d6ec4db6b15ce05bddae056040e5f1ef4
Mae Beale <maebeale@gmail.com>
2020-05-10 00:32:21 -0400

    LocationType CRUD

---
+++ app/controllers/location_types_controller.rb
@@ -0,0 +1,49 @@
+class LocationTypesController < ApplicationController
+  before_action :set_location_type, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @location_types = LocationType.all
+  end
+
+  def show
+  end
+
+  def new
+    @location_type = LocationType.new
+  end
+
+  def edit
+  end
+
+  def create
+    @location_type = LocationType.new(location_type_params)
+
+    if @location_type.save
+      redirect_to @location_type, notice: 'Location type was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @location_type.update(location_type_params)
+      redirect_to @location_type, notice: 'Location type was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @location_type.destroy
+    redirect_to location_types_url, notice: 'Location type was successfully destroyed.'
+  end
+
+  private
+    def set_location_type
+      @location_type = LocationType.find(params[:id])
+    end
+
+    def location_type_params
+      params.require(:location_type).permit(:name, :description, :display_to_public)
+    end
+end

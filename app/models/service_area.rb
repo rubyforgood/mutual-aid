dffa824ef5ec97716f5dffd33c5a4174b712e1fc
Mae Beale <maebeale@gmail.com>
2020-05-10 12:31:59 -0400

    Move sort logic out of controller and in to model since it requires more work to access/handle translations

--- app/models/service_area.rb
+++ app/models/service_area.rb
@@ -7,6 +7,8 @@ class ServiceArea < ApplicationRecord
   belongs_to :organization, optional: true
   belongs_to :location, class_name: "Location", inverse_of: :service_areas, foreign_key: :location_id
 
+  has_many :mobility_string_translations, inverse_of: :translatable, class_name: "MobilityStringTranslation", foreign_key: :translatable_id
+
   has_many :asks, class_name: "Ask", foreign_key: "service_area_id", inverse_of: :service_area
   has_many :offers, class_name: "Offer", foreign_key: "service_area_id", inverse_of: :service_area
   has_many :listings
@@ -16,6 +18,13 @@ class ServiceArea < ApplicationRecord
   validates :name, presence: true
 
   accepts_nested_attributes_for :location
+
+  scope :order_by_translated_name, -> (locale=:en){
+    includes(:mobility_string_translations).references(:mobility_string_translations).
+    where("mobility_string_translations.locale = ?", locale.to_s).
+    where("mobility_string_translations.key = ?", 'name').
+    order(MobilityStringTranslation.arel_table["value"].lower.asc)
+  }
 
   TYPES = %w[pod neighborhood region county]
 

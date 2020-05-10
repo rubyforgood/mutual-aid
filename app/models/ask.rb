9e851e6d045d6e8b8c891efafbbfacd1fb258f29
Mae Beale <maebeale@gmail.com>
2020-05-10 10:05:47 -0400

    Change from .tags -> .all_tags_list (per docs this is more comprehensive)

--- app/models/ask.rb
+++ app/models/ask.rb
@@ -2,4 +2,8 @@ class Ask < Listing
   belongs_to :service_area, inverse_of: :asks
 
   scope :unmatched, ->() { includes(:matches_as_receiver).references(:matches_as_receiver).where("matches.provider_id IS NULL") }
+
+  def name
+    all_tags_to_s
+  end
 end

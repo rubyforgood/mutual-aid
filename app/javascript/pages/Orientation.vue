f995d8df0112edea4b7f10f588aa37f3386a8469
Mae Beale <maebeale@gmail.com>
2020-05-09 22:18:14 -0400

    Change welcome/landing_page orientation - Try 2-column layout for text and buttons - Streamlined desired actions to one button per section - Adding 'Both' section for now (this is discussion point)

--- app/javascript/pages/Orientation.vue
+++ app/javascript/pages/Orientation.vue
@@ -1,41 +1,48 @@
 <template>
   <div class='orientation section'>
-    <h1 class='title'>Welcome!</h1>
+    <div class='columns'>
+      <div class='column'>
+        <h1 class='title'>Welcome!</h1>
+      </div>
+    </div>
     <div class='columns'>
       <div class='column'>
         <AboutSection title='What is Mutual-Aid?' description="Mutual-aid is a movement. It's about connecting people with each other in a time of need. It's people helping people, because we can."/>
-      </div>
-      <div class='column'>
         <AboutSection title='Who are we?' description="We're your regional mutual aid group. Learn more"/>
+        <AboutSection title="Where do you fit in?" description="Good neighbors share. Asking is a gift to the giver. Whatever your individual case may be, we're here to help you make connections." />
       </div>
-    </div>
-    <div class='has-text-center' style='text-align: center;'>
-       <AboutSection title="Where do you fit in?" description="Good neighbors share. Asking is a gift to the giver. Whatever your individual case may be, we're here to help you make connections." />
-    </div>
-    <div class='columns'>
       <div class='column'>
         <div>
           <h2 class='subtitle is-3'>Need some help?</h2>
           <div class='action-buttons'>
-            <b-button type='is-primary' outlined>View offers</b-button>
             <b-button type='is-primary'>Submit an Ask</b-button>
             <br>
-            <span class=''><a href='/community_resources'>Browse resources</a></span>
+            <span class=''><a href='/listings'>View offers and resources</a></span>
           </div>
         </div>
-      </div>
-      <div class='column'>
         <div>
+          <br><br>
           <h2 class='subtitle is-3'>Want to help?</h2>
           <div class='action-buttons'>
             <b-button type='is-primary'>View Asks</b-button>
-            <b-button type='is-primary' outlined>Submit an Offer</b-button>
             <br>
-            <span class=''><a href='/community_resources/new'>Share a community resource</a></span>
+            <span class=''><a href='/listings'>Submit an offer</a></span>
+            <br>
+            <span class=''><a href='/community_resources/new'>Submit a community resource</a></span>
+            <br>
+            <span class=''><a href='/announcements/new'>Submit an announcement</a></span>
             <br>
             <span class=''><a href='/donations/new'>Donate money</a></span> | <span class=''><a href='/donations/new'>Donate goods</a></span>
             <br>
             <span class=''><a href='/volunteers/new'>Volunteer</a></span>
+          </div>
+        </div>
+
+        <div>
+          <br><br>
+          <h2 class='subtitle is-3'>Want to do both?</h2>
+          <div class='action-buttons'>
+            <b-button type='is-primary'>Create a Profile</b-button>
           </div>
         </div>
       </div>
@@ -58,6 +65,6 @@ export default {
 
 <style scoped>
   .column div{
-    text-align:center;
+    text-align:left;
   }
 </style>

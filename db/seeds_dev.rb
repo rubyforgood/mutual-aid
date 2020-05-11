4d79c8ae38e81b470dbbe3d7e2735db72d108735
Mae Beale <maebeale@gmail.com>
2020-05-11 00:33:19 -0400

    Convert constant call to relation iteration and tags -> tag_list

--- db/seeds_dev.rb
+++ db/seeds_dev.rb
@@ -7,21 +7,21 @@ phone_contact_method = ContactMethod.where(ContactMethod.arel_table[:name].match
 person = Person.where(preferred_contact_method: "email", email: "personsemail@example.com(opens in new tab)").first_or_create!
 person_2 = Person.where(preferred_contact_method: "phone", phone: "123-123-1234").first_or_create!
 # asks for person
-Category::DEFAULT_TAGS.sample(4).each do |tag|
+Category.all.pluck(:name).sample(4).each do |tag|
   ask = Ask.where(person: person, service_area: ServiceArea.take).create! # TODO - make this idempotent
-  ask.tags << tag
+  ask.tag_list << tag
   ask.save!
 end
 # offers for person
-Category::DEFAULT_TAGS.sample(1).each do |tag|
+Category.all.pluck(:name).sample(1).each do |tag|
   offer = Offer.where(person: person, service_area: ServiceArea.take).create! # TODO - make this idempotent
-  offer.tags << tag
+  offer.tag_list << tag
   offer.save!
 end
 # offers for person_2
-Category::DEFAULT_TAGS.sample(4).each do |tag|
+Category.all.pluck(:name).sample(4).each do |tag|
   offer = Offer.where(person: person_2, service_area: ServiceArea.take).create! # TODO - make this idempotent
-  offer.tags << tag
+  offer.tag_list << tag
   offer.save!
 end
 # matches
@@ -47,19 +47,23 @@ log_2 = CommunicationLog.where(
   delivery_channel: "autoemail", 
   delivery_status: "completed"
 ).first_or_create!(sent_at: Time.now - 1.day)
+
+
 # submissions
 Ask.all.sample(2).each do |ask|
-  submission = Submission.where(
-    person: ask.person, service_area: ask.service_area, form_name: "ask_form", privacy_level_requested: "none", body: ask.to_json).create! # TODO - make this idempotent
-  ask.tags << tag
-  ask.save!
+  Submission.where(person: ask.person, service_area: ask.service_area, form_name: "ask_form", privacy_level_requested: "none", body: ask.to_json).create! # TODO - make this idempotent
 end
+Offer.all.sample(2).each do |offer|
+  Submission.where(person: offer.person, service_area: offer.service_area, form_name: "ask_form", privacy_level_requested: "none", body: offer.to_json).create! # TODO - make this idempotent
+end
+
 # offers for person
-Category::DEFAULT_TAGS.sample(1).each do |tag|
+Category.all.pluck(:name).sample(1).each do |tag|
   offer = Offer.where(person: person, service_area: ServiceArea.take).create! # TODO - make this idempotent
-  offer.tags << tag
+  offer.tag_list << tag
   offer.save!
 end
+
 # donations
 # custom_form_questions
 # feedbacks

758d923008974c4a9214a316075af33bc6790c32
Mae Beale <maebeale@gmail.com>
2020-05-11 07:18:07 -0400

    Uncomment the tests I'd skipped over earlier

--- spec/interactors/save_listing_spec.rb
+++ spec/interactors/save_listing_spec.rb
@@ -1,52 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe SaveListing do
-  # let(:service_area)    { create :service_area }
-  # let(:contact_method)  { create :contact_method_email }
-  # subject(:interaction) { SaveListing.run params }
-  #
-  # let(:params) {{
-  #   service_area: service_area.id,
-  #   type: 'Offer',
-  #   person: {
-  #     email: 'we@together.coop',
-  #     name: 'Marcus Garvey',
-  #     preferred_contact_method: contact_method.id,
-  #     location: {
-  #       city: 'Lansing',
-  #       state: 'MI',
-  #     },
-  #   },
-  # }}
-  #
-  # context 'with valid params' do
-  #   # it { is_expected.to be_valid } # TODO fix this
-  #
-  #   it 'create listing and nested records' do
-  #     skip # TODO fix this
-  #     expect { interaction }
-  #       .to  change(Listing,  :count).by(1)
-  #       .and change(Person,   :count).by(1)
-  #       .and change(Location, :count).by(1)
-  #   end
-  # end
-  #
-  # context 'with bad listing params' do
-  #   before { params[:type] = '' }
-  #
-  #   it { is_expected.to be_invalid }
-  #
-  #   it 'does not create any records' do
-  #     expect { interaction }
-  #       .to  change(Listing,  :count).by(0)
-  #       .and change(Person,   :count).by(0)
-  #       .and change(Location, :count).by(0)
-  #   end
-  #
-  #   it 'gathers errors from nested objects' do
-  #     skip # TODO fix this
-  #     expect(interaction.errors.full_messages).to eq ["Type can't be blank"]
-  #   end
-  # end
+  let(:service_area)    { create :service_area }
+  let(:contact_method)  { create :contact_method_email }
+  subject(:interaction) { SaveListing.run params }
+
+  let(:params) {{
+    service_area: service_area.id,
+    type: 'Offer',
+    person: {
+      email: 'we@together.coop',
+      name: 'Marcus Garvey',
+      preferred_contact_method: contact_method.id,
+      location: {
+        city: 'Lansing',
+        state: 'MI',
+      },
+    },
+  }}
+
+  context 'with valid params' do
+    # it { is_expected.to be_valid } # TODO fix this
+
+    it 'create listing and nested records' do
+      skip # TODO fix this
+      expect { interaction }
+        .to  change(Listing,  :count).by(1)
+        .and change(Person,   :count).by(1)
+        .and change(Location, :count).by(1)
+    end
+  end
+
+  context 'with bad listing params' do
+    before { params[:type] = '' }
+
+    it { is_expected.to be_invalid }
+
+    it 'does not create any records' do
+      expect { interaction }
+        .to  change(Listing,  :count).by(0)
+        .and change(Person,   :count).by(0)
+        .and change(Location, :count).by(0)
+    end
+
+    it 'gathers errors from nested objects' do
+      skip # TODO fix this
+      expect(interaction.errors.full_messages).to eq ["Type can't be blank"]
+    end
+  end
 end
 

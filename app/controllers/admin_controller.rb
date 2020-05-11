b77e7fb75256461c38ef10dacffbf8d53a9df43a
Mae Beale <maebeale@gmail.com>
2020-05-11 19:53:41 -0400

    Update completion percentage to only count 1 match per ask

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,7 +3,7 @@ class AdminController < ApplicationController
   before_action :authenticate_user!, except: [:new, :create]
 
   def landing_page
-    @match_count = Match.any? ? Match.all.length : 1
+    @match_count = Match.any? ? Match.pluck(:receiver_id).uniq.length : 1
     @ask_count = Ask.any? ? Ask.all.length : 1
     @percent_asks_fulfilled = ((@match_count.to_f/@ask_count.to_f).to_f * 100).to_i
     @asks_unmatched_count = Ask.unmatched.length

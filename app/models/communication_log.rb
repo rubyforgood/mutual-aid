908981092a34bac1c88eb646259587f4dfc792c3
Mae Beale <maebeale@gmail.com>
2020-05-13 13:35:59 -0400

    Merge conflicts

--- app/models/communication_log.rb
+++ app/models/communication_log.rb
@@ -3,8 +3,8 @@ class CommunicationLog < ApplicationRecord
   belongs_to :match, optional: true
   belongs_to :created_by, optional: true, class_name: "User", foreign_key: "created_by_id"
 
-<<<<<<< HEAD
   DEFAULT_DELIVERY_STATUS = "completed"
+
   AUTO_DELIVERY_CHANNEL = "autoemail"
   DELIVERY_CHANNELS = [AUTO_DELIVERY_CHANNEL, "email", "text", "call", "snailmail"]
 

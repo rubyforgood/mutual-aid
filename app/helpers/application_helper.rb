79833ad5260bdda32af80d8a2b2de1d20aa3866e
Mae Beale <maebeale@gmail.com>
2020-05-13 10:24:48 -0400

    Remove conditional on 'show' so show_button can be added to other model show pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,16 +19,14 @@ module ApplicationHelper
   end
 
   def show_button(resource, button_text="View", icon_class="fa fa-eye", margin_class=nil)
-    if resource && action_name != "show"
-      resource_class = resource.class
-      if resource_class.superclass != ApplicationRecord
-        resource = resource.becomes(resource.class.superclass)
-      end
-      link_to(polymorphic_path(resource),
-              title: action_name || button_text + " " + controller_path || resource_class.to_s,
-              class: "button show-button #{margin_class}") do
-        "<span class='#{icon_class}'></span><span style='padding-left: 0.25em'> #{button_text}</span>".html_safe
-      end
+    resource_class = resource.class
+    if resource_class.superclass != ApplicationRecord
+      resource = resource.becomes(resource.class.superclass)
+    end
+    link_to(polymorphic_path(resource),
+            title: action_name || button_text + " " + controller_path || resource_class.to_s,
+            class: "button show-button #{margin_class}") do
+      "<span class='#{icon_class}'></span><span style='padding-left: 0.25em'> #{button_text}</span>".html_safe
     end
   end
 end

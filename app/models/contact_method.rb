9c9eeb9ecc34f0231f3edc2f00d392c7cbcb2595
Howard M. Miller <hmiller@customink.com>
2020-05-13 23:33:13 -0400

    expand available filtering on browse page

--- app/models/contact_method.rb
+++ app/models/contact_method.rb
@@ -2,6 +2,7 @@ class ContactMethod < ApplicationRecord
   has_many :people, dependent: :restrict_with_error
 
   scope :enabled, -> { where enabled: true }
+  scope :as_filter_types, -> { enabled.distinct(:name) }
 
   def label
     I18n.t field, scope: [:models, :contact_method, :labels]
---
+++ app/models/contribution_type.rb
@@ -0,0 +1,13 @@
+ContributionType = Struct.new(:id, :name, :model)
+class ContributionType
+  TYPES = [new('Ask', 'Ask', Ask), new('Offer', 'Offer', Offer)].freeze
+
+  def self.as_filter_types
+    TYPES
+  end
+
+  def self.where(id: [], name: [])
+    ids = (id + name).flatten
+    ids.map {|i| TYPES.find {|t| t.id == i }}
+  end
+end

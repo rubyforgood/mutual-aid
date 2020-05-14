e33524dbd977519ac72085bf3e47c25bceaf2c55
Mae Beale <maebeale@gmail.com>
2020-05-14 06:58:01 -0400

    I'm in a weird world between branches...

--- app/blueprints/contribution_blueprint.rb
+++ app/blueprints/contribution_blueprint.rb
@@ -5,9 +5,10 @@ class ContributionBlueprint < Blueprinter::Base
   association :contact_types, blueprint: DefaultBlueprint do |contribution, _options|
     [contribution.person.preferred_contact_method]
   end
-  association :urgency, blueprint: DefaultBlueprint do |contribution, _options|
-    UrgencyLevel.find(contribution.urgency_level_id)
-  end
+  # commenting out bc I branched of browse-improvements
+  # association :urgency, blueprint: DefaultBlueprint do |contribution, _options|
+  #   UrgencyLevel.find(contribution.urgency_level_id)
+  # end
   fields :title, :description
   field :created_at do |contribution, _options|
     contribution.created_at.to_formatted_s(:iso8601)

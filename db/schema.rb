61d98144105279cad7329f498ae228710d6bf88f
Howard M. Miller <hmiller@customink.com>
2020-05-13 23:35:25 -0400

    add incomplete implementation of urgency levels to contribution filters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_031357) do
+ActiveRecord::Schema.define(version: 2020_05_14_013541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -148,11 +148,13 @@ ActiveRecord::Schema.define(version: 2020_05_13_031357) do
     t.bigint "service_area_id", null: false
     t.bigint "location_id"
     t.bigint "submission_id"
+    t.bigint "urgency_level_id"
     t.index ["location_id"], name: "index_listings_on_location_id"
     t.index ["person_id"], name: "index_listings_on_person_id"
     t.index ["service_area_id"], name: "index_listings_on_service_area_id"
     t.index ["submission_id"], name: "index_listings_on_submission_id"
     t.index ["tags"], name: "index_listings_on_tags", using: :gin
+    t.index ["urgency_level_id"], name: "index_listings_on_urgency_level_id"
   end
 
   create_table "location_types", force: :cascade do |t|

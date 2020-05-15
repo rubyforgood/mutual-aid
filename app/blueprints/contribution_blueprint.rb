8fdab083db46e435365f580b610a707b9a446819
Mae Beale <maebeale@gmail.com>
2020-05-14 22:39:25 -0400

    Uncomment needed blueprint code

--- app/blueprints/contribution_blueprint.rb
+++ app/blueprints/contribution_blueprint.rb
@@ -5,10 +5,9 @@ class ContributionBlueprint < Blueprinter::Base
   association :contact_types, blueprint: DefaultBlueprint do |contribution, _options|
     [contribution.person.preferred_contact_method]
   end
-  # commenting out bc I branched of browse-improvements
-  # association :urgency, blueprint: DefaultBlueprint do |contribution, _options|
-  #   UrgencyLevel.find(contribution.urgency_level_id)
-  # end
+  association :urgency, blueprint: DefaultBlueprint do |contribution, _options|
+    UrgencyLevel.find(contribution.urgency_level_id)
+  end
   fields :title, :description
   field :created_at do |contribution, _options|
     contribution.created_at.to_formatted_s(:iso8601)

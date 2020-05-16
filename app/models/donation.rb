8925851a7dd15d3b42d312794e526a7a25c0ff1a
Mae Beale <maebeale@gmail.com>
2020-05-16 19:20:58 -0400

    Update Donation model/views w CHANNELS (and view decorators like yes_no and nubmer_to_currency)

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -6,6 +6,8 @@ class Donation < ApplicationRecord
 
   accepts_nested_attributes_for :person
 
+  CHANNELS = ["cold call", "website", "email campaign"]
+
   scope :this_month, -> { where("donations.created_at >= ? AND donations.created_at <= ?",
                                 Time.zone.now.beginning_of_month, Time.zone.now.end_of_month) }
 end

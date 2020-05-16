81d6807e834b1448416b90aa6e1c0e200a8c0e46
Mae Beale <maebeale@gmail.com>
2020-05-16 19:11:40 -0400

    Update communication_log fields - Remove the from_type and to_type fields, that were dying on the vine - Add outgoing boolean to distinguish whether the log is an incoming communication or outgoing - Inherit from ContactMethod for delivery_method assn, since this was essentially duplicative w that data - Add `autoemail` scope on ContacMethod to find methods that are/aren't system logs (autoemails)

--- app/models/contact_method.rb
+++ app/models/contact_method.rb
@@ -2,6 +2,8 @@ class ContactMethod < ApplicationRecord
   has_many :people, inverse_of: :preferred_contact_method, foreign_key: :preferred_contact_method_id, dependent: :restrict_with_error
 
   scope :enabled, -> { where enabled: true }
+  scope :autoemail, -> (boolean){ where( boolean ? arel_table[:name].lower.eq("autoemail") : arel_table[:name].lower.not_eq("autoemail") ) }
+
   scope :as_filter_types, -> { enabled.distinct(:name) }
 
   def label

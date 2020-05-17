656ff4cb2c0301de8a86354e0d1a39ec66ad78fc
Mae Beale <maebeale@gmail.com>
2020-05-17 09:44:52 -0400

    Add feedback button to nav (that captures href)

---
+++ app/javascript/components/forms/FeedbackButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <form :action="action" method="get">
+    <input type="hidden" :name="123"/><!-- # TODO get this to capture page_url window.location.href page_url=123-->
+    <button class="button is-outlined">
+      <slot />
+    </button>
+  </form>
+</template>
+
+<script>
+import AuthTokenInput from './AuthTokenInput'
+
+export default {
+  components: {AuthTokenInput},
+  props: {
+    action: String,
+  },
+  computed: {
+    authenticityToken() {
+      return document.querySelector('meta[name="csrf-token"]').content
+    },
+  },
+}
+</script>

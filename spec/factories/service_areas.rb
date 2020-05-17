1002a245ff0546c6fdce7d8b73b4a3254c56dd60
Mae Beale <maebeale@gmail.com>
2020-05-16 21:37:20 -0400

    Add required fields to service_area factory

--- spec/factories/service_areas.rb
+++ spec/factories/service_areas.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :service_area do
     association :location
+    association :organization
+    service_area_type { Faker::Name.name }
     parent { nil }
     name { Faker::Name.name }
   end

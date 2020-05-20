f97a852c9d76e37ef355bf6ed84efd4ac4223290
Mae Beale <maebeale@gmail.com>
2020-05-20 10:24:35 -0400

    Revert "Add more types to UrgencyLevel"
    This reverts commit a35b0f039ae2dbb75336867d6f5c98baafb5aea9.

--- app/models/urgency_level.rb
+++ app/models/urgency_level.rb
@@ -3,7 +3,7 @@ UrgencyLevel = Struct.new(:id, :name, :other)
 # We can convert this to some other type of class, such as an ActiveRecord model
 # I'm hard-coding the options for now, though
 class UrgencyLevel
-  TYPES = [new(1, 'Within 1-2 days'), new(2, 'Within 3-5 days'), new(3, 'About a week'), new(4, 'Anytime')].freeze
+  TYPES = [new(1, 'Within 1-2 days'), new(2, 'I can wait a week')].freeze
 
   def self.as_filter_types
     TYPES
@@ -18,5 +18,4 @@ class UrgencyLevel
     ids = [id].flatten
     ids.map {|i| TYPES.select {|t| t.id == i }}
   end
-
 end

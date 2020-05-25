5ef8bede10705e7207458265eb4c66915a54706f
Mae Beale <maebeale@gmail.com>
2020-05-25 18:58:30 -0400

    WIP: small tweaks for demo in 2 min

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,12 +22,12 @@ module ApplicationHelper
     end
   end
 
-  def show_button(resource, button_text="View", icon_class="fa fa-eye", margin_class=nil, button_text_class=nil)
+  def show_button(resource, button_text="View", icon_class="fa fa-eye", margin_class=nil, button_text_class=nil, params={})
     resource_class = resource.class
     if resource_class != Person && (resource_class.superclass != ApplicationRecord)
       resource = resource.becomes(resource.class.superclass)
     end
-    link_to(polymorphic_path(resource),
+    link_to(polymorphic_path(resource, params),
             title: action_name || button_text + " " + controller_path || resource_class.to_s,
             class: "button show-button #{margin_class}") do
       "<span class='#{icon_class}'></span><span class='#{button_text_class}' style='padding-left: 0.25em'> #{button_text}</span>".html_safe

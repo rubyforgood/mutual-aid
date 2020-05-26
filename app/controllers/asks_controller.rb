312a8e27f7ba5a5b3f25b1a11c30722d2502da86
exbinary <exbinary@gmail.com>
2020-05-26 14:22:44 -0400

    Drop AsksControlller#updated for now (not working)

--- app/controllers/asks_controller.rb
+++ app/controllers/asks_controller.rb
@@ -17,16 +17,6 @@ class AsksController < PublicController
     end
   end
 
-  def update # for rails-side edits
-    @listing = Listing.find(params[:id])
-    if @listing.save
-      redirect_to listings_path, notice: 'Listing was successfully updated.'
-    else
-      set_form_dropdowns
-      render :edit
-    end
-  end
-
   private
     def submission_params
       params[:submission].tap do |p|

4ba30bb31e56890c69c082fc23c7eed08ab3b383
exbinary <exbinary@gmail.com>
2020-05-28 12:41:45 -0400

    Remove unused Reform forms

--- app/forms/listing_form.rb
+++
@@ -1,5 +0,0 @@
-# TODO: Remove this when we replace listings_controller actions with offer/ask controller actions
-class ListingForm < Reform::Form
-  collection :tags
-  property :location, form: LocationForm
-end

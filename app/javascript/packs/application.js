6d5c9bc348c115761413a39f2371374527786d8e
Shane Moore <shanewmoore@gmail.com>
2020-06-14 19:55:50 -0400

    Fix PG config, fontawesome, and dockerfile

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 // require("@rails/activestorage").start()
 // require("channels")
+require("@fortawesome/fontawesome-free/js/all")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference

6ff0f83b77292325f23bce39f9b4fa3ea51fba91
exbinary <exbinary@gmail.com>
2020-07-03 17:00:17 -0400

    Move image processing over to webpack
    * Change existing image_tags to image_pack_tag
    * Extract small partial to show org logo
    * Note: the change in config/webpack/environment.js is actually cleanup
      from d0399ec which added `app/assets` as a resolved_path to webpacker
      such that we no longer needed the `images` alias

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,17 +1,12 @@
 const path = require('path')
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
+const vue = require('./loaders/vue') // todo: flatten this
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 
 environment.config.merge({
-  resolve: {
-    alias: {
-      images:  path.resolve(__dirname, '../../app/assets/images'),
-    },
-  },
   output: {
     // Exposes the export from last pack (application.js) as a global var.
     // https://webpack.js.org/configuration/output/#outputlibrary

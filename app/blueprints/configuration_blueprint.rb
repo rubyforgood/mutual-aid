dfa2149862a741ff054526b341565ef82bbfeeb5
exbinary <exbinary@gmail.com>
2020-07-24 14:15:56 -0400

    Use visible scope instead of additional association in CategoryBlueprint

--- app/blueprints/configuration_blueprint.rb
+++ app/blueprints/configuration_blueprint.rb
@@ -1,6 +1,6 @@
 class ConfigurationBlueprint < Blueprinter::Base
   association :categories, blueprint: CategoryBlueprint, view: :with_subcategories do
-    Category.visible.roots.includes(:visible_subcategories)
+    Category.visible.roots.includes(:categories)
   end
 
   association :contact_methods, blueprint: ContactMethodBlueprint do

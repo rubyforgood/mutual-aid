b5818ffc18c6fe2fab2adf163b4d979a211a9bd1
Keith Walters <keith.walters@cattywamp.us>
2020-09-12 12:29:55 -0400

    WIP: Import location and Service Area for Community Resource
    This adds the location and service area models to the community resource being
    imported.

--- app/models/importers/community_resource_importer.rb
+++ app/models/importers/community_resource_importer.rb
@@ -8,7 +8,29 @@ class Importers::CommunityResourceImporter < Importers::BaseImporter
     comm_resource = CommunityResource.create! name: row["name"], website_url: row["website_url"], facebook_url: row["facebook_url"], phone: row["phone"], description: row["description"]
 
     comm_resource.organization = Organization.create! name: row["organization_name"]
-    # comm_resource.location = Location.create! street_address: row["street"]
+    business_location = LocationType.find_or_create_by(name: "business")
+    comm_resource.location = Location.create! do |location|
+      location.street_address = row["street"]
+      location.city = row["city"]
+      location.state = row["state"]
+      location.zip = row["zip"]
+      location.county = row["county"]
+      location.location_type = business_location
+    end
+
+    service_location_type = LocationType.find_or_create_by(name: "service_area")
+    comm_resource.service_area = ServiceArea.create! do |service_area|
+      service_area.name = "Test Service Area"
+      service_area.service_area_type = "unknown"
+      service_area.organization = comm_resource.organization
+      service_area.location = Location.create! do |location|
+        location.location_type = service_location_type
+        location.city = row["city"]
+        location.state = row["state"]
+        location.county = row["county"]
+      end
+    end
+
     comm_resource.save
   end
 end
--- spec/models/importers/community_resource_importer_spec.rb
+++ spec/models/importers/community_resource_importer_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe Importers::CommunityResourceImporter do
 
   let(:single_record) {
     <<~CSV
-        category_name,name,organization_name,street,city,state,zip,county,service_area_name,service_area_counties,service_area_town_names,phone,website_url,facebook_url,publish_from,publish_until,is_created_by_admin,is_approved,youtube_identifier,description
-        food,Food Vouchers,WIC,123 Main Street,Springfield,XY,12345,Queens County,,,,818-555-9876,https://example.com,https://facebook.com/sample,2020-09-05,,TRUE,TRUE,,A long description of text.
+      category_name,name,organization_name,street,city,state,zip,county,service_area_name,service_area_counties,service_area_town_names,phone,website_url,facebook_url,publish_from,publish_until,is_created_by_admin,is_approved,youtube_identifier,description
+      food,Food Vouchers,WIC,123 Main Street,Springfield,XY,12345,Queens County,,,,818-555-9876,https://example.com,https://facebook.com/sample,2020-09-05,,TRUE,TRUE,,A long description of text.
     CSV
   }
 
@@ -62,11 +62,84 @@ RSpec.describe Importers::CommunityResourceImporter do
       expect(resource.organization.name).to eq "WIC"
     end
 
-    # failing bc needs location type but we dont know what type it is
-    # it 'creates a CommunityResource with a location street' do
-    #   subject.import_string single_record
-    #   resource = CommunityResource.last
-    #   expect(resource.location.street_address).to eq "123 Main Street"
-    # end
+    it 'creates a CommunityResource with a business location type' do
+      subject.import_string single_record
+      resource = CommunityResource.last
+      expect(resource.location.location_type.name).to eq "business"
+    end
+
+    it 'creates a CommunityResource with a location street' do
+      subject.import_string single_record
+      resource = CommunityResource.last
+      expect(resource.location.street_address).to eq "123 Main Street"
+    end
+
+    it 'creates a CommunityResource with a location city' do
+      subject.import_string single_record
+      resource = CommunityResource.last
+      expect(resource.location.city).to eq "Springfield"
+    end
+
+    it 'creates a CommunityResource with a location state' do
+      subject.import_string single_record
+      resource = CommunityResource.last
+      expect(resource.location.state).to eq "XY"
+    end
+
+    it 'creates a CommunityResource with a location zip' do
+      subject.import_string single_record
+      resource = CommunityResource.last
+      expect(resource.location.zip).to eq "12345"
+    end
+
+    it 'creates a CommunityResource with a location county' do
+      subject.import_string single_record
+      resource = CommunityResource.last
+      expect(resource.location.county).to eq "Queens County"
+    end
+
+    context "when service area location is not provided" do
+      it "sets the service area name from the community resource location" do
+        pending "What should the name be set to if it's not provided?"
+        subject.import_string single_record
+        resource = CommunityResource.last
+        expect(resource.service_area.name).to eq "Unknown"
+      end
+
+      it "sets the service area location city from the community resource location" do
+        subject.import_string single_record
+        resource = CommunityResource.last
+        expect(resource.service_area.location.city).to eq "Springfield"
+      end
+
+      it "sets the service area location state from the community resource location" do
+        subject.import_string single_record
+        resource = CommunityResource.last
+        expect(resource.service_area.location.state).to eq "XY"
+      end
+
+      it "sets the service area location county from the community resource location" do
+        subject.import_string single_record
+        resource = CommunityResource.last
+        expect(resource.service_area.location.county).to eq "Queens County"
+      end
+
+      it "sets the service area location type" do
+        subject.import_string single_record
+        resource = CommunityResource.last
+        expect(resource.service_area.location.location_type.name).to eq "service_area"
+      end
+    end
+
+    context "when service area location is provided" do
+      let(:single_record_with_service_area) {
+        <<~CSV
+          category_name,name,organization_name,street,city,state,zip,county,service_area_name,service_area_counties,service_area_town_names,phone,website_url,facebook_url,publish_from,publish_until,is_created_by_admin,is_approved,youtube_identifier,description
+          food,Food Vouchers,WIC,123 Main Street,Springfield,XY,12345,Queens County,Sunny Side,"Queens County, Brooklyn",,818-555-9876,https://example.com,https://facebook.com/sample,2020-09-05,,TRUE,TRUE,,A long description of text.
+        CSV
+      }
+
+
+    end
   end
 end

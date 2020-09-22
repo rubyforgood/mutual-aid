8034ebbfe468228ff21f2025648859ca10282867
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2020-09-22 10:24:16 -0400

    Setup peer-to-peer mailer file

---
+++ app/mailers/peer_to_peer_mailer.rb
@@ -0,0 +1,8 @@
+require "#{Rails.root}/app/helpers/application_helper.rb"
+include ApplicationHelper
+
+class PeerToPeerMailer < ApplicationMailer
+  def peer_to_peer_email
+    # more details to go here
+  end
+end

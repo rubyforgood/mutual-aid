af6277d7de84c6a22f29639922e5b71fd81fb096
Maricris Nonato <maricris.nonato@powerhrg.com>
2020-09-23 22:21:36 +0800

    Updating map to render sample marker

--- app/javascript/pages/browse/MapBrowser.vue
+++ app/javascript/pages/browse/MapBrowser.vue
@@ -1,21 +1,21 @@
 <template>
   <section class="MapBrowser">
     <mapbox
-        access-token="pk.eyJ1IjoibXV0dWFsLWFpZC1hcHAiLCJhIjoiY2tmZTBvd3UwMDBhbTJ4cDlic2JmMWZoaiJ9.rWscBjdl1SMT5N0yekIJYg"
-        :map-options="{
-          style: 'mapbox://styles/mapbox/streets-v11',
-          center: [-75.1635262, 39.9527237],
-          zoom: 9,
-        }"
-        :geolocate-control="{
-          show: true,
-          position: 'top-left',
-        }"
-        @map-load="loaded"
-        @map-zoomend="zoomend"
-        @map-click:points="clicked"
-        @geolocate-error="geolocateError"
-        @geolocate-geolocate="geolocate"
+      access-token="pk.eyJ1IjoibXV0dWFsLWFpZC1hcHAiLCJhIjoiY2tmZTBvd3UwMDBhbTJ4cDlic2JmMWZoaiJ9.rWscBjdl1SMT5N0yekIJYg"
+      :map-options="{
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-75.1635262, 39.9527237],
+        zoom: 9,
+      }"
+      :geolocate-control="{
+        show: true,
+        position: 'top-left',
+      }"
+      @map-load="loaded"
+      @map-zoomend="zoomend"
+      @map-click:points="clicked"
+      @geolocate-error="geolocateError"
+      @geolocate-geolocate="geolocate"
       />
   </section>
 </template>
@@ -27,15 +27,30 @@ export default {
   components: { Mapbox },
   methods: {
     loaded(map) {
+      var geojson = {
+        'type': 'FeatureCollection',
+        'features': [
+          {
+            'type': 'Feature',
+            'properties': {
+              'message': 'Foo',
+              'icon': 'harbor',
+              'iconSize': [60, 60]
+            },
+            'geometry': {
+              'type': 'Point',
+              'coordinates': [-75.1635262, 39.9527237]
+            }
+          },
+        ]
+      }
+
       map.addLayer({
         id: 'points',
         type: 'symbol',
         source: {
           type: 'geojson',
-          data: {
-            type: 'FeatureCollection',
-            features: [],
-          },
+          data: geojson,
         },
         layout: {
           'icon-image': '{icon}-15',
@@ -45,6 +60,23 @@ export default {
           'text-anchor': 'top',
         },
       })
+
+      geojson.features.forEach(function (marker) {
+        var el = document.createElement('div');
+        el.className = 'marker';
+        el.style.backgroundImage =
+        'url(https://placekitten.com/g/5/5/)';
+        el.style.width = marker.properties.iconSize[0] + 'px';
+        el.style.height = marker.properties.iconSize[1] + 'px';
+
+        el.addEventListener('click', function () {
+          window.alert(marker.properties.message);
+        });
+
+        new mapboxgl.Marker(el)
+          .setLngLat(marker.geometry.coordinates)
+          .addTo(map);
+      });
     },
     zoomend(map, e) {
       console.log('Map zoomed')

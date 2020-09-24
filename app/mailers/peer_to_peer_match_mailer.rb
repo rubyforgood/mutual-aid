5e3747cf7928dde8d27533d7b5efa8594aff5692
Anil Cherukuri <anil.cherukuri03@gmail.com>
2020-09-24 19:15:51 -0400

    Fixes indentation in P2P mailer

--- app/mailers/peer_to_peer_match_mailer.rb
+++ app/mailers/peer_to_peer_match_mailer.rb
@@ -3,10 +3,10 @@ class PeerToPeerMatchMailer < ApplicationMailer
     peer_email_address = contribution.person.email
     peer_alias = peer_to_peer_match_params[:peer_alias]
     email_subject = if contribution.ask?
-                "#{peer_alias} is offering to meet your ask"
-              elsif contribution.offer? # TODO: check if community resource when added
-                "#{peer_alias} would like to accept your offer"
-              end
+                      "#{peer_alias} is offering to meet your ask"
+                    elsif contribution.offer? # TODO: check if community resource when added
+                      "#{peer_alias} would like to accept your offer"
+                    end
     @email_body = peer_to_peer_match_params[:message]
     mail(to: peer_email_address, subject: email_subject)
   end

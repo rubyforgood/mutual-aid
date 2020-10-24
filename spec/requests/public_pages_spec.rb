b32769581c95e0987469fd4fc2c2807df6535d2f
Ilya Shaplyko <shaplyko.ilya@gmail.com>
2020-10-24 21:09:38 +0300

    add version endpoint for badgegen

--- spec/requests/public_pages_spec.rb
+++ spec/requests/public_pages_spec.rb
@@ -31,4 +31,16 @@ RSpec.describe "/", type: :request do
     # TODO assert
   end
 
+  describe 'GET /version' do
+    let(:version) { JSON.parse(File.read(Rails.root.join('package.json'))).dig('version') }
+
+    it 'renders a successful response json' do
+      get version_url
+      expect(response).to be_successful
+      json = JSON.parse(response.body)
+      expect(json['subject']).to eq('version')
+      expect(json['status']).to eq(version)
+      expect(json['color']).to eq('blue')
+    end
+  end
 end

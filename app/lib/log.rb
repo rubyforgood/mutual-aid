5c1a6d6db1e00a8e9f9b1149e48a080cf6c5fef8
Sherilyn Moline <smoline@gmail.com>
2020-10-24 20:12:40 -0400

    Style/MethodDefParentheses

--- app/lib/log.rb
+++ app/lib/log.rb
@@ -6,7 +6,7 @@ module Log
   # logging only the Exception's message. We _should_ be able to
   # configure the rails logger with our own formatter, but that
   # appears to take considerable finagling.
-  def self.error exception, preamble = nil
+  def self.error(exception, preamble = nil)
     lines = [
       preamble,
       "#{exception.message} (#{exception.class})",

4cb15f37c780a072f5347a05bfd12ecf8af6bfa7
Ilya Shaplyko <shaplyko.ilya@gmail.com>
2020-10-25 15:44:04 +0300

    #756 User is able to select filters quickly

--- spec/javascript/pages/browse/Filters.spec.js
+++ spec/javascript/pages/browse/Filters.spec.js
@@ -1,4 +1,4 @@
-import {mount} from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import Filters from 'pages/browse/Filters'
 import filterTypes from '../../../../lib/filterCategories.json'
 
@@ -9,11 +9,42 @@ describe('Filters', function () {
       result = event
     }
     const wrapper = mount(Filters, {
-      propsData: {filterTypes: filterTypes},
-      listeners: {change: clickHandler},
+      propsData: { filterTypes: filterTypes },
+      listeners: { change: clickHandler },
     })
-    const checkbox = wrapper.find('input[type=checkbox]')
+    const checkbox = wrapper.findAll('input[type=checkbox]').at(2)
     checkbox.trigger('click')
     assert.deepEqual(result, [filterTypes[0].filters[0].id])
+  })
+
+  it('toggle-filters works', function () {
+    var result = {}
+    const clickHandler = function (event, trash) {
+      result = event
+    }
+    const wrapper = mount(Filters, {
+      propsData: { filterTypes: filterTypes },
+      listeners: { change: clickHandler },
+    })
+    const checkbox = wrapper.find(`#toggle-filters-${filterTypes[0].name}`)
+    checkbox.trigger('click')
+    assert.deepEqual(result, filterTypes[0].filters.map(f=>f.id))
+  })
+
+  it('toggle-all-filters works', function () {
+    var result = {}
+    const clickHandler = function (event, trash) {
+      result = event
+    }
+    const wrapper = mount(Filters, {
+      propsData: { filterTypes: filterTypes },
+      listeners: { change: clickHandler },
+    })
+    const checkbox = wrapper.find('#toggle-all-filters')
+    checkbox.trigger('click')
+    const allFilters = [].concat(
+      ...filterTypes.map((fType) => fType.filters.map((filter) => filter.id))
+    )
+    assert.deepEqual(result.sort(), allFilters.sort())
   })
 })

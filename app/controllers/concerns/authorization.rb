e596fbb7f6d70f9f0016f7964405f3a1dab4d163
exbinary <exbinary@gmail.com>
2021-01-17 23:41:26 -0500

    Exclude Devise controllers from pundit auth verification
    Ref: https://github.com/varvet/pundit/issues/113#issuecomment-36357606

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -3,10 +3,10 @@ module Authorization
 
   included do
     include Pundit
-    
-    after_action :verify_authorized, except: :index
-    after_action :verify_policy_scoped, only: :index
-    
+
+    after_action :verify_authorized, except: :index, unless: :devise_controller?
+    after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
+
     rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   end
 

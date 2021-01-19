rubyforgood/mutual-aid

commit 7fa64948d13df24ddb52b8887e1035543951a22e

Flatten ClaimContribution params
Simpler. Also drops unnecessary strong_params from controller.

app/controllers/claims_controller.rb
@@ -14,19 +14,14 @@ class ClaimsController < ApplicationController
   end
 
   def create
-    ClaimContribution.run!(
+    ClaimContribution.run!(params.merge(
       contribution: params[:contribution_id],
-      claim_params: claim_params.to_h,
       current_user: current_user
-    )
+    ))
     redirect_to contribution_path(params[:contribution_id]), notice: 'Claim was successful!'
   end
 
   private
-
-  def claim_params
-    params.require(:claim).permit(:peer_alias, :email, :message)
-  end
 
   def current_person
     current_user.person

app/interactions/claim_contribution.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 class ClaimContribution < ActiveInteraction::Base
+  string :peer_alias
+  string :email
+  string :message
   record :contribution, class: Listing
-  hash :claim_params do
-    string :peer_alias
-    string :email
-    string :message
-  end
   object :current_user, class: User
 
   def execute
@@ -24,16 +22,16 @@ class ClaimContribution < ActiveInteraction::Base
     if current_user.person.blank?
       Person.create!(new_person_params)
     else
-      current_user.person.update!(email: claim_params[:email])
+      current_user.person.update!(email: email)
     end
   end
 
   def new_person_params
     {
-      name: claim_params[:peer_alias],
+      name: peer_alias,
       preferred_contact_method: ContactMethod.email,
       user: current_user,
-      email: claim_params[:email]
+      email: email,
     }
   end
 
@@ -44,8 +42,8 @@ class ClaimContribution < ActiveInteraction::Base
   def email_peer
     EmailPeer.run!(
       contribution: contribution,
-      peer_alias: claim_params[:peer_alias],
-      message: claim_params[:message],
+      peer_alias: peer_alias,
+      message: message,
       user: current_user
     )
   end

spec/interactions/claim_contribution_spec.rb
@@ -8,17 +8,16 @@ RSpec.describe ClaimContribution do
 
   let(:contribution) { create(:listing) }
   let(:user) { create(:user)}
-  let(:claim_params) do
-    {
-      peer_alias: 'alias',
-      email: 'user_email@example.com',
-      message: 'message'
-    }
-  end
+  let(:peer_alias) { 'alias' }
+  let(:email) { 'user_email@example.com' }
 
-  subject(:interaction) { ClaimContribution.run!(contribution: contribution.id,
-                                                 claim_params: claim_params,
-                                                 current_user: user) }
+  subject(:interaction) { ClaimContribution.run!(
+    contribution: contribution.id,
+    current_user: user,
+    email: email,
+    peer_alias: peer_alias,
+    message: 'message',
+  )}
 
   it "matches contribution and sends an email to peer", :aggregate_failures do
     allow_any_instance_of(ClaimContribution).to receive(:add_person_details)
@@ -33,8 +32,8 @@ RSpec.describe ClaimContribution do
     it "creates a person" do
       interaction
 
-      expect(user.reload.person).to have_attributes(name: claim_params[:peer_alias],
-                                                    email: claim_params[:email],
+      expect(user.reload.person).to have_attributes(name: peer_alias,
+                                                    email: email,
                                                     preferred_contact_method: ContactMethod.email,
                                                     user: user)
     end
@@ -46,7 +45,7 @@ RSpec.describe ClaimContribution do
     it "updates the person email address" do
       interaction
 
-      expect(user.reload.person).to have_attributes(email: claim_params[:email], user: user)
+      expect(user.reload.person).to have_attributes(email: email, user: user)
     end
   end
 end

commit eee97401f2adcbaa752184004c7c03eaa0c5405f

Fix missing params error when claiming
I introduced a regression in 7fa64948 by not accounting for params being
nested under params[:claim]. Really speaks to the need for integration
tests!

app/controllers/claims_controller.rb
@@ -16,7 +16,7 @@ class ClaimsController < ApplicationController
   end
 
   def create
-    ClaimContribution.run!(params.merge(
+    ClaimContribution.run!(params[:claim].merge(
       contribution: params[:contribution_id],
       current_user: current_user
     ))
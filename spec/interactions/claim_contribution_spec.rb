97efe88112776c2f3d6386dc4f61387884183b81
exbinary <exbinary@gmail.com>
2021-02-08 23:41:17 -0500

    Don't allow email override in ClaimContribution
    Use current_user.email instead. This mitigates possible abuse
    because the email of a logged in user has to be verified, whereas
    earlier, any email could've been entered.

--- spec/interactions/claim_contribution_spec.rb
+++ spec/interactions/claim_contribution_spec.rb
@@ -7,45 +7,32 @@ RSpec.describe ClaimContribution do
   end
 
   let(:contribution) { create(:listing) }
-  let(:user) { create(:user)}
-  let(:peer_alias) { 'alias' }
-  let(:email) { 'user_email@example.com' }
+  let(:user) { create(:user, :with_person, email: 'user_email@example.com') }
 
-  subject(:interaction) { ClaimContribution.run!(
-    contribution: contribution.id,
-    current_user: user,
-    email: email,
-    peer_alias: peer_alias,
-    message: 'message',
-  )}
+  before do
+    ClaimContribution.run!(
+      contribution: contribution.id,
+      current_user: user,
+      peer_alias: 'alias',
+      message: 'message',
+    )
+  end
 
   it "matches contribution and sends an email to peer", :aggregate_failures do
-    allow_any_instance_of(ClaimContribution).to receive(:add_person_details)
-
-    interaction
-
     expect(MatchContribution).to have_received(:run!)
     expect(EmailPeer).to have_received(:run!)
   end
 
   context "when current user has no person record" do
+    let(:user) { create(:user, email: 'user_email@example.com', person: nil) }
+
     it "creates a person" do
-      interaction
-
-      expect(user.reload.person).to have_attributes(name: peer_alias,
-                                                    email: email,
-                                                    preferred_contact_method: ContactMethod.email,
-                                                    user: user)
-    end
-  end
-
-  context "when current has a person" do
-    let(:user) { create(:user, :with_person)}
-
-    it "updates the person email address" do
-      interaction
-
-      expect(user.reload.person).to have_attributes(email: email, user: user)
+      expect(user.reload.person).to have_attributes(
+        user: user,
+        email: user.email,
+        name: 'alias',
+        preferred_contact_method: ContactMethod.email,
+      )
     end
   end
 end

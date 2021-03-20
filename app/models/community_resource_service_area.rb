44bb4fc5c804e03bc3c1fdf5cd841a062ee446be
exbinary <exbinary@gmail.com>
2021-03-20 12:52:06 -0400

    Comments only: update schema comments on a/m/community_resource.rb

--- app/models/community_resource_service_area.rb
+++ app/models/community_resource_service_area.rb
@@ -2,3 +2,22 @@ class CommunityResourceServiceArea < ApplicationRecord
   belongs_to :community_resource
   belongs_to :service_area
 end
+
+# == Schema Information
+#
+# Table name: community_resource_service_areas
+#
+#  id                    :bigint           not null, primary key
+#  community_resource_id :bigint           not null
+#  service_area_id       :bigint           not null
+#
+# Indexes
+#
+#  index_community_resource_service_areas_on_community_resource_id  (community_resource_id)
+#  index_community_resource_service_areas_on_service_area_id        (service_area_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (community_resource_id => community_resources.id)
+#  fk_rails_...  (service_area_id => service_areas.id)
+#

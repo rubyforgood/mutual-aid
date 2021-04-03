b68799a2f4ff067cf7003091933671d7e007ca47
exbinary <exbinary@gmail.com>
2021-04-02 23:43:58 -0400

    Use `string` filter instead of `record` for `location_type`
    `active_interaction` doesn't coerce an empty string value to the default
    value on `record` filters[1]. This causes the form to break on any form
    where `location_type` is optional (eg CommunityResources#create).
    
    We hadn't run into this on the Ask|Offer form because we only show the
    location_type input once the user provides an address, and then rely on
    the browser to enforce that it's populated.
    
    [1] https://github.com/AaronLasseigne/active_interaction/issues/503

--- app/forms/location_form.rb
+++ app/forms/location_form.rb
@@ -3,7 +3,7 @@
 class LocationForm < BaseForm
   with_options default: nil do
     integer :id
-    record  :location_type
+    integer :location_type
     string  :street_address
     string  :neighborhood
     string  :city
@@ -17,7 +17,14 @@ class LocationForm < BaseForm
     return nil if inputs.values.all?(&:blank?)
 
     Location.find_or_new(id).tap do |location|
-      location.attributes = given_inputs
+      location.attributes = given_inputs_without_location_type
+      location.location_type_id = location_type if given?(:location_type)
     end
   end
+
+  private
+
+    def given_inputs_without_location_type
+      given_inputs.reject {|key| key == :location_type }
+    end
 end

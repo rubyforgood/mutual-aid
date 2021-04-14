1e4ca747fecdf7f2d38735d045c5f6569ccc466a
exbinary <exbinary@gmail.com>
2021-04-14 01:54:46 -0400

    Layout/FirstHashElementIndentation: fix violations
    Led to some cleanup of hash definitions.
    
    https://docs.rubocop.org/rubocop/cops_layout.html#layoutfirsthashelementindentation

--- app/models/custom_form_question.rb
+++ app/models/custom_form_question.rb
@@ -11,20 +11,20 @@ class CustomFormQuestion < ApplicationRecord
   has_many :mobility_string_translations, inverse_of: :translatable, class_name: 'MobilityStringTranslation', foreign_key: :translatable_id
 
   INPUT_TYPES_AND_STORAGE = {
-      'date' => 'date_response',
-      'datetime' => 'datetime_response',
-      'info_text' => nil,
-      'integer' => 'integer_response',
+    'date' => 'date_response',
+    'datetime' => 'datetime_response',
+    'info_text' => nil,
+    'integer' => 'integer_response',
 
-      'multiselect-checkboxes' => 'array_response',
-      'multiselect-dropdown' => 'array_response',
-      'radio' => 'boolean_response',
+    'multiselect-checkboxes' => 'array_response',
+    'multiselect-dropdown' => 'array_response',
+    'radio' => 'boolean_response',
 
-      'file' => 'string_response',
-      'select' => 'string_response',
-      'string' => 'string_response',
-      'textarea' => 'string_response', # for some reason textarea are being stored as string_response
-      'youtube_video_id' => 'string_response',
+    'file' => 'string_response',
+    'select' => 'string_response',
+    'string' => 'string_response',
+    'textarea' => 'string_response', # for some reason textarea are being stored as string_response
+    'youtube_video_id' => 'string_response',
   }
 
   scope :for_form, ->(form) { joins(:form_questions).where(form_questions: {form: form}) }

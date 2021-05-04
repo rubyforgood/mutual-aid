30de713adf67443a6b408ed47b6cf5e91773474a
exbinary <exbinary@gmail.com>
2021-05-04 14:44:48 -0400

    Fix some rubocop violations surfaced from merging main

--- app/blueprints/person_blueprint.rb
+++ app/blueprints/person_blueprint.rb
@@ -4,7 +4,7 @@ class PersonBlueprint < Blueprinter::Base
   identifier :id
 
   fields :name, :email, :phone, :skills
-  association  :preferred_contact_method, blueprint: ContactMethodBlueprint
+  association :preferred_contact_method, blueprint: ContactMethodBlueprint
 
   view :with_location do
     association :location, blueprint: LocationBlueprint, view: :with_location_type

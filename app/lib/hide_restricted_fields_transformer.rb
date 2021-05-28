7cf6f129d1b12a2ba32969d447eaa11dd5be32ea
exbinary <exbinary@gmail.com>
2021-05-28 11:21:11 -0700

    Fail fast in blueprint authorization; simplify spec
    As of the last commit, BlueprintAuthorization has to be explicitly
    requested, so the transformer can assume the presence of a policy that
    implements restricted_attributes and can fail fast if that's not the
    case.
    
    Also simplifies the spec accordingly.

--- app/lib/hide_restricted_fields_transformer.rb
+++ app/lib/hide_restricted_fields_transformer.rb
@@ -1,8 +1,6 @@
 class HideRestrictedFieldsTransformer < Blueprinter::Transformer
   def transform(hash, object, options)
     policy = Pundit.policy(options[:current_user], object)
-    return hash unless policy && policy.respond_to?(:restricted_attributes)
-
     policy.restricted_attributes.each do |attr|
       hash[attr] = nil
     end
--- spec/lib/hide_restricted_fields_transformer_spec.rb
+++ spec/lib/hide_restricted_fields_transformer_spec.rb
@@ -5,65 +5,45 @@ RSpec.describe HideRestrictedFieldsTransformer do
     FactoryBot.build(:user, id: 42)
   end
 
-  let(:object) { model.new(name: 'person-name', location: FactoryBot.build(:location)) }
+  context 'when serializing with authorization' do
+    let(:object) { FactoryBot.build(:person, id: 21, name: 'person-name', location: FactoryBot.build(:location)) }
 
-  subject(:serialize) do
-    PersonBlueprint.render_as_hash(object, current_user: current_user)
-  end
-
-  context 'when serializing an object without a policy' do
-    let(:model) do
-      Class.new(Person) do
-        def policy_class
-          nil
-        end
-      end
+    let(:serialized_hash) do
+      PersonBlueprint.render_as_hash(object, current_user: current_user)
     end
 
-    it { is_expected.to include(name: 'person-name') }
-  end
+    it 'nullifies restricted attributes' do
+      expect(serialized_hash).to include(name: nil)
+    end
 
-  context 'when serializing an object with a policy that implements `restricted_attributes`' do
-    let(:model) { Person }
-
-    it { is_expected.to include(name: nil) }
+    it 'leaves public attributes as is' do
+      expect(serialized_hash).to include(id: 21)
+    end
 
     context 'with a specified view' do
-      subject(:serialize) do
+      let(:serialized_hash) do
         PersonBlueprint.render_as_hash(object, view: :with_location, current_user: current_user)
       end
 
-      it { is_expected.to include(name: nil) }
-      it { is_expected.to include(location: nil) }
+      it 'nullifies restricted attributes in the root object' do
+        expect(serialized_hash).to include(name: nil)
+      end
+
+      it 'nullifies restricted attributes in views' do
+        expect(serialized_hash).to include(location: nil)
+      end
     end
   end
 
-  context 'when serializing an object with a policy that does not implement `restricted_attributes`' do
-    let(:model) { Person }
-
-    before do
-      PersonPolicy.alias_method(:orig_restricted_attributes, :restricted_attributes)
-      PersonPolicy.undef_method(:restricted_attributes)
-    end
-
-    after do
-      PersonPolicy.alias_method(:restricted_attributes, :orig_restricted_attributes)
-      PersonPolicy.undef_method(:orig_restricted_attributes)
-    end
-
-    specify 'serialization works transparently' do
-      expect(PersonPolicy).not_to respond_to(:restricted_attributes)
-      expect { serialize }.not_to raise_error
-    end
-  end
-
-  context 'when serializing the parent of an object with restricted_attributes' do
+  context 'when serializing the parent of a blueprint with authorization' do
     let(:parent) { FactoryBot.build(:ask, person: FactoryBot.build(:person)) }
 
-    subject do
-      ListingBlueprint.render_as_hash(parent, view: :with_person, current_user: current_user)[:person]
+    let(:serialized_hash) do
+      ListingBlueprint.render_as_hash(parent, view: :with_person, current_user: current_user)
     end
 
-    it { is_expected.to include(name: nil) }
+    it 'nullifies restricted attributes in the nested object' do
+      expect(serialized_hash[:person]).to include(name: nil)
+    end
   end
 end

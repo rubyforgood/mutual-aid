e139258bff18688056a232a383a5d3af47574996
Bob Mazanec <bob@wizeservices.com>
2021-11-30 14:19:00 -0600

    Use ApplicationPolicy#can_admin?
    Per comments on #996, use ApplicationPolicy#can_admin? rather than
    duplicating it in NavBarPolicy#is_admin?

--- app/policies/nav_bar_policy.rb
+++ app/policies/nav_bar_policy.rb
@@ -3,15 +3,8 @@ class NavBarPolicy < ApplicationPolicy
     visible = Set.new
     visible << (user ? 'Logout' : 'Login')
     visible << 'Sign Up' unless user
-    visible << 'Feedback' if is_admin?(user)
     visible.merge %w[Glossary Contributions] if system_settings.peer_to_peer?
-    visible.merge %w[Contributions Matches Admin] if can_admin?
+    visible.merge %w[Contributions Feedback Matches Admin] if can_admin?
     visible.to_a
-  end
-
-  private
-
-  def is_admin?(user)
-    user && (user.admin_role? || user.sys_admin_role?)
   end
 end

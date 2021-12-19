254bda1be24d3308b3457e76aac3e090a39618ed
exbinary <exbinary@gmail.com>
2021-12-19 10:51:49 -0500

    Document our pundit <=> blueprinter integration

--- app/blueprints/blueprint_authorization.rb
+++ app/blueprints/blueprint_authorization.rb
@@ -1,3 +1,21 @@
+# Provides authorization during serialization,
+# leveraging existing pundit policies
+#
+# To add authorization to a blueprint (serializer):
+# 1. Implement a `restricted_attributes` method in the related policy.
+# 2. `extend BlueprintAuthorization` in the blueprint.
+#
+# See PersonBlueprint for a working example.
+#
+# Note that Blueprinter already supports custom
+# [Transforms](https://github.com/procore/blueprinter#example).
+# These could be a simpler integration point, but currently need to be
+# applied both at the top level of a blueprint and in each nested view.
+# We felt this would be too easy to miss.
+#
+# This issue has been noted in blueprinter here:
+# https://github.com/procore/blueprinter/pull/247/files
+#
 module BlueprintAuthorization
   def self.extended(blueprint)
     blueprint.class_eval do
